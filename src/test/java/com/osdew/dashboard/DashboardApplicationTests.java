be8d8017c84a3f2ebddee7caf8bb9afdb0681d8a
hjhwang <hhjoong@gmail.com>
2022-11-11 14:37:18 +0900

    1. init commit

---
+++ src/test/java/com/osdew/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osdew.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
